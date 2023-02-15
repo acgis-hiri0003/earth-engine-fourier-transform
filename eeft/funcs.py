320b1b422ade7bfe910329427f948c358c08d527
Ryan Hamilton <ryangilberthamilton@gmail.com>
2023-02-15 14:59:31 -0500

    added base functions

---
+++ eeft/funcs.py
@@ -0,0 +1,34 @@
+import math
+
+from typing import List, Callable
+
+import ee
+
+
+def add_harmonics(freq: List[int], cos_names: List[str], sin_names: List[str]) -> Callable:    
+    def add_harmonics_wrapper(element: ee.Image):
+        frequencies = ee.Image.constant(freq)
+        time = ee.Image(element).select('t')
+        cosines = time.multiply(frequencies).cos().rename(cos_names)
+        sines = time.multiply(frequencies).sin().rename(sin_names)
+        return element.addBands(cosines).addBands(sines)
+    return add_harmonics_wrapper
+
+
+def add_ndvi(nir: str, red: str) -> Callable:
+    def add_ndvi_wrapper(element: ee.Image):
+        return element.addBands(element.normalizedDifference([nir, red]).rename('NDVI'))
+    return add_ndvi_wrapper
+
+
+def add_constant(element: ee.Image):
+    return element.addBands(ee.Image(1))
+
+
+def add_time(omega: float = 1.5) -> Callable:
+        def add_time_inner(image: ee.Image):
+            date = image.date()
+            years = date.difference(ee.Date('1970-01-01'), 'year')
+            time_radians = ee.Image(years.multiply((2 * omega) * math.pi))
+            return image.addBands(time_radians.rename('t').float())
+        return add_time_inner
